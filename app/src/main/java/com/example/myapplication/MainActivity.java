d586ed69c1315b3b423a65d50831ec0dc0ba8fed
2022070319 <wisecow703@hanyang.ac.kr>
2023-07-08 02:48:49 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "KakaoLogin";
+    private View loginButton, logoutButton;
+    private TextView nickName;
+    private ImageView profileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        KakaoSdk.init(this,"b1ce2fb14f0364dbe72795025c0129fd");
+
+
+        ImageView kakao_login_button = findViewById(R.id.login);
+
+
+        Function2<OAuthToken,Throwable, Unit> callback =new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            // 콜백 메서드 ,
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                Log.e(TAG,"CallBack Method");
+                //oAuthToken != null 이라면 로그인 성공
+                if(oAuthToken!=null){
+                    // 토큰이 전달된다면 로그인이 성공한 것이고 토큰이 전달되지 않으면 로그인 실패한다.
+                    Log.d("Success", "!");
+
+                }else {
+                    //로그인 실패
+                    Log.e(TAG, "invoke: login fail" );
+                }
+
+                return null;
+            }
+        };
+
+
+        kakao_login_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
+                    login();
+                }
+                else{
+                    accountLogin();
+                }
+            }
+        });
+
+    }
+
+    public void login(){
+        String TAG = "login()";
+        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
+            if (error != null) {
+                Log.e(TAG, "로그인 실패", error);
+            } else if (oAuthToken != null) {
+                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+                getUserInfo();
+            } else {
+                Log.i(TAG, "sdasdasda ");
+            }
+            return null;
+        });
+    }
+
+    public void accountLogin(){
+        String TAG = "accountLogin()";
+        UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this,(oAuthToken, error) -> {
+            if (error != null) {
+                Log.e(TAG, "로그인 실패", error);
+            } else if (oAuthToken != null) {
+                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+                getUserInfo();
+            }
+            return null;
+        });
+    }
+
+    public void getUserInfo(){
+        String TAG = "getUserInfo()";
+        UserApiClient.getInstance().me((user, meError) -> {
+            if (meError != null) {
+                Log.e(TAG, "사용자 정보 요청 실패", meError);
+            } else {
+                System.out.println("로그인 완료");
+                Log.i(TAG, user.toString());
+                {
+                    Log.i(TAG, "사용자 정보 요청 성공" +
+                            "\n회원번호: "+user.getId() +
+                            "\n이메일: "+user.getKakaoAccount().getEmail());
+                }
+                Account user1 = user.getKakaoAccount();
+                System.out.println("사용자 계정" + user1);
+            }
+            return null;
+        });
+    }
+
+}
